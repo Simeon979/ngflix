701c1dfe7ff2d232d11765def4f0973f4f01f333
Simeon <adegbolasimeon@gmail.com>
2019-10-16 02:33:39 +0100

    add movie details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
