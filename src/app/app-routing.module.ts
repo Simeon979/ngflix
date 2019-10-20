21f678faca478d34f00e1c54f31b6803e019746a
Simeon <adegbolasimeon@gmail.com>
2019-10-20 14:22:58 +0100

    set up basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesListComponent } from './movies/movies-list/movies-list.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "movies", component: MoviesListComponent },
+  { path: "movies/:id", component: MovieDetailsComponent },
+  { path: "", redirectTo: "/movies", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
